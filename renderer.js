80ff31a996130a77744e458f980e99c430b64eba
RFrancois6 <romain57890@orange.fr>
2020-12-08 13:58:09 +0100

    inscription

--- renderer.js
+++ renderer.js
@@ -4,27 +4,3 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
-
-$(document).ready(function () {
-    $('#submitButton').click(function () {
-      var firstName = $('#firstName').val()
-      var lastName = $('#lastName').val()
-      var email = $('#email').val()
-      var password = $('#password').val()
-      var confirmPassword = $('#confirmPassword').val()
-      if (
-        firstName == '' ||
-        lastName == '' ||
-        email == '' ||
-        password == '' ||
-        confirmPassword == ''
-      ) {
-        alert('Please fill all fields !')
-      } else if (password.length < 8) {
-        alert('Password should at least 8 character in length !')
-      } else if (!password.match(confirmPassword)) {
-        alert("Your passwords don't match !")
-      }
-    })
-  })
-  
