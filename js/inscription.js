a41fa4001c76bad83cbf178035b944bbc7ce40b1
Matthieu <matth.fra@gmail.com>
2021-01-04 11:47:31 +0100

    passage du mock à l'API pour l'inscription

--- js/inscription.js
+++ js/inscription.js
@@ -40,15 +40,19 @@ form.addEventListener("submit", function (e) {
 
   if (readyToAdd(validPass(password.value, confirmPassword.value), noEmpty())) {
     http(
-      "http://localhost:3000/users",
+      "http://duelquizz-php/api/user/register",
       "POST",
       {
-        name: firstName.value + " " + lastName.value,
+        firstName: firstName.value,
+        lastName: lastName.value,
         email: email.value,
         password: password.value,
-        points: 0,
       },
-      (window.location.href = "../html/connection.html")
+      redirect
     );
   }
 });
+
+function redirect() {
+  window.location.href = "../html/connexion.html";
+}
